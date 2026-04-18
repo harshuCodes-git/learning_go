ef731f99075a41110c120255711fb087abd9db7d
harshuCodes-git <1032210414@tcetmumbai.in>
2026-04-18 23:40:53 +0530

    The things are added in the system

---
+++ 02_single_values/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// int 
+	fmt.Println(2)
+
+	// string 
+	fmt.Println("Harsh Dubey")
+
+	//bool 
+	fmt.Println(true)
+
+	//float 
+	fmt.Println(10.5)
+
+	//artimatic operation 
+	fmt.Println(7.0/2)
+
+
+
+
+
+
+
+
+}
