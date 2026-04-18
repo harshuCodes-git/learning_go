14114d422177389bef270d5caf6180b1a44eae76
harshuCodes-git <1032210414@tcetmumbai.in>
2026-04-18 23:41:46 +0530

    The 3 leatures are done in this flow

---
+++ 03_variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name="harsh"
+	fmt.Println(name)
+
+	var val= 2 
+	fmt.Println(val)
+
+	// sorthand syntax 
+
+	value:="golang"
+	fmt.Println(value)
+}
